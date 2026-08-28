d52503db3cc6a1489b29a707d05918886a8098b8
WooJiMin03 <dnwlals7857@naver.com>
2026-08-28 12:12:27 +0900

    [Passed] 겹치지 않게 선분 고르기 (61ms, 16MB)

---
+++ trail4/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,31 @@
+n = int(input())
+x1, x2 = [], []
+for _ in range(n):
+    a, b = map(int, input().split())
+    x1.append(a)
+    x2.append(b)
+line = []
+best = 0
+def select_line(start,count):
+    global best
+    for i in range(start,n):
+        a,b =x1[i],x2[i] 
+        if(safe(a,b)):
+            line.append([a,b])
+            if(count+1>best):
+                best=count+1
+            select_line(i+1,count+1)
+            line.pop()
+        else:
+            select_line(i+1,count)
+    return
+# Please write your code here.
+
+def safe(a,b):
+    for a1,b1 in line:
+        if(a1<=a<=b1 or a1<=b<=b1 or (a<=a1 and b>=b1)):
+            return False
+    return True
+
+select_line(0,0)
+print(best)
