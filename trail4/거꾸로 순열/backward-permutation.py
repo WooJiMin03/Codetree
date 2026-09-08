fe451e034969d91b545a79ac79d8b3429b0bd52b
WooJiMin03 <dnwlals7857@naver.com>
2026-09-08 13:55:48 +0900

    [Passed] 거꾸로 순열 (235ms, 26MB)

--- trail4/거꾸로 순열/backward-permutation.py
+++ trail4/거꾸로 순열/backward-permutation.py
@@ -1,14 +1,19 @@
-import itertools
-
 n = int(input())
 
-# N부터 1까지 내림차순으로 리스트 생성
 nums = [i for i in range(n, 0, -1)]
-
-# permutations는 입력된 리스트의 순서를 기준으로 순열을 생성하므로 
-# 역사전순으로 바로 출력됨
-for p in itertools.permutations(nums):
-    print(*p)
-
-
+cur_num = []
+visited = [False]*n
+def dfs():
+    global visited
+    if(len(cur_num)==n):
+        print(*cur_num)
+        return
+    for i in range(n):
+        if(visited[i] == False):
+            cur_num.append(nums[i])
+            visited[i] = True
+            dfs()
+            cur_num.pop()
+            visited[i] = False
+dfs()        
 # Please write your code here.
