42613baaaa25068e479a50f56d08d7f18b4556f3
WooJiMin03 <dnwlals7857@naver.com>
2026-09-11 00:45:02 +0900

    [Passed] 안전 지대 (49ms, 16MB)

--- trail4/안전 지대/comfort-zone.py
+++ trail4/안전 지대/comfort-zone.py
@@ -3,44 +3,38 @@ sys.setrecursionlimit(10000)
 
 N, M = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
-
+best = 1
 max_height = 0
+ground_count = 0
+directions = [[1,0],[0,1],[-1,0],[0,-1]] #x,y
+def dfs(x,y,k,visited):
+    for ax,ay in directions:
+        nx = x+ax
+        ny = y+ay
+        if(0<=nx<M and 0<=ny<N):
+            if(not visited[ny][nx] and grid[ny][nx]>k):
+                visited[ny][nx]=True
+                dfs(nx,ny,k,visited)
+
 for i in range(N):
     for j in range(M):
-        if grid[i][j] > max_height:
+        if(max_height < grid[i][j]):
             max_height = grid[i][j]
 
-directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-def dfs(y, x, k, visited):
-    for dy, dx in directions:
-        ny, nx = y + dy, x + dx
-        
-
-        if 0 <= ny < N and 0 <= nx < M:
-            if not visited[ny][nx] and grid[ny][nx] > k:
-                visited[ny][nx] = True
-                dfs(ny, nx, k, visited)
-
-best_k = 1
-max_zones = -1
-
-for k in range(1, max_height + 1):
-    visited = [[False] * M for _ in range(N)]
-    zones = 0
-    
+for K in range(1,max_height+1):
+    visited = [[False]*M for _ in range(N)]
+    count = 0
     for i in range(N):
         for j in range(M):
-           
-            if grid[i][j] > k and not visited[i][j]:
-                visited[i][j] = True
-                dfs(i, j, k, visited)
-                zones += 1  
-                
-    if zones > max_zones:
-        max_zones = zones
-        best_k = k
+            if(grid[i][j] > K and not visited[i][j]):
+                visited[i][j]=True
+                dfs(j,i,K,visited)
+                count+=1
+    if(ground_count < count):
+        ground_count=count
+        best = K
 
-print(best_k, max_zones)
+
+print(best,ground_count)
 
 # Please write your code here.
