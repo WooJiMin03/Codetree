b5698be3ed09ef148fe04982659444fc57d7000b
WooJiMin03 <dnwlals7857@naver.com>
2026-09-26 23:40:06 +0900

    [Passed] 최대 감소 부분 수열 (59ms, 17MB)

---
+++ trail4/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -0,0 +1,13 @@
+n = int(input())
+m = list(map(int, input().split()))
+dp = [1]*n
+for i in range(n):
+    for j in range(i):
+        if(m[i]<m[j]):
+            dp[i]=max(dp[i],dp[j]+1)
+best = 0
+for i in range(n):
+    if(dp[i]>best):
+        best=dp[i]
+print(best)
+# Please write your code here.
