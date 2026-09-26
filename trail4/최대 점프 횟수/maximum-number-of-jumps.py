a1a38c36ca81b0ddc57384a406d1ba09ef948a04
WooJiMin03 <dnwlals7857@naver.com>
2026-09-27 00:11:14 +0900

    [Passed] 최대 점프 횟수 (65ms, 17MB)

--- trail4/최대 점프 횟수/maximum-number-of-jumps.py
+++ trail4/최대 점프 횟수/maximum-number-of-jumps.py
@@ -1,10 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [-float('inf')]*n
+min=-float('inf')
+dp = [min]*n
 dp[0]=0
 for i in range(1,n):
     for j in range(0,i):
-        if(dp[j]==-float('inf')):
+        if(dp[j]==min):
             continue
         if(j+arr[j]>=i):
             dp[i]=max(dp[i],dp[j]+1)
